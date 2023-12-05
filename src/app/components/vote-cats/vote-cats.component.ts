2e1e5e85611e7ee476b42b00bf7a27812d70515c
Benmalek Houcine <benmalek.houcine@gmail.com>
2023-12-05 01:17:46 +0100

    Cats vote view

--- src/app/components/vote-cats/vote-cats.component.ts
+++ src/app/components/vote-cats/vote-cats.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ListCatsService } from '../../services/list-cats/list-cats.service';
+
 
 @Component({
   selector: 'app-vote-cats',
@@ -8,5 +12,23 @@ import { Component } from '@angular/core';
   styleUrl: './vote-cats.component.scss'
 })
 export class VoteCatsComponent {
+  voteScore: number = 5;
+  firstCat: any = {};
+  secondCat: any = {};
 
+  constructor(private router: Router, readonly listCatsService: ListCatsService) {}
+
+  ngOnInit(): void {
+    this.listCatsService.getCats().subscribe({
+      next: res => {
+        this.firstCat = res.images[0];
+        this.secondCat = res.images[1];
+        // console.log('iciii : ', res);
+      }
+    });
+  }
+
+  goToHome() {
+    this.router.navigateByUrl('/');
+  }
 }
